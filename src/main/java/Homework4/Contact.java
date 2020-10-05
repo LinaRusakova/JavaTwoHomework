4dac5ea30aa3c215a79036aabc9b7e70b6459865
IgorTeslenko <igor.teslenko@gmail.com>
2020-10-05 22:36:23 +0200

    Homework 4 minor fixes

--- src/main/java/Homework4/Contact.java
+++ src/main/java/Homework4/Contact.java
@@ -5,25 +5,15 @@ import javafx.collections.ObservableList;
 
 public class Contact {
     private final String name;
-    private boolean isOnline;
-    private String lastMessage;
+//    private boolean isOnline;
+//    private String lastMessage;
     private final ObservableList<String> messages;
 
     public Contact(String name) {
         this.name = name;
-        this.isOnline = true;
-        this.lastMessage = null;
+//        this.isOnline = true;
+//        this.lastMessage = null;
         messages = FXCollections.observableArrayList();
-    }
-
-    public void addMessage(String userName, String message) {
-        this.messages.add(String.format("%s: %s",userName, message));
-        this.lastMessage = String.format("%s: %s",userName, message);
-    }
-
-    public void addMessage(String message) {
-        this.messages.add(String.format("%s: %s",this.name, message));
-        this.lastMessage = String.format("%s: %s",this.name, message);
     }
 
     public ObservableList<String> getMessages() {
@@ -32,5 +22,15 @@ public class Contact {
 
     public String getName() {
         return name;
+    }
+
+    public void addMessage(String userName, String message, String dateTime) {
+        this.messages.add(String.format("%s (%s):%n%s", userName, dateTime, message));
+//        this.lastMessage = String.format("%s: %s",userName, message);
+    }
+
+    public void addMessage(String message, String dateTime) {
+        this.messages.add(String.format("%s (%s):%n%s", this.name, dateTime, message));
+//        this.lastMessage = String.format("%s: %s",this.name, message);
     }
 }
--- src/main/java/Homework4/Controller.java
+++ src/main/java/Homework4/Controller.java
@@ -2,18 +2,16 @@ package Homework4;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableListBase;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Controller {
 
@@ -28,6 +26,8 @@ public class Controller {
 
 
     public void initialize() {
+
+        //Введеные вручную данные для проверки работы, от этого блока впоследствии надо избавиться
         ObservableList<Contact> contacts = FXCollections.observableArrayList(
                 new Contact("Mama"),
                 new Contact("Papa"),
@@ -41,7 +41,11 @@ public class Controller {
             contactList.add(contact.getName());
         }
 
-        contacts.get(0).addMessage("Привет!");
+        contacts.get(0).addMessage("Привет!", getCurrentDate());
+        contacts.get(1).addMessage("Как дела?", getCurrentDate());
+        contacts.get(2).addMessage("Ты тут?", getCurrentDate());
+        contacts.get(3).addMessage("Дай денег, плз, зарплату задерживают, а мелкой надо штаны зимние купить.", getCurrentDate());
+        //Конец данных инициализации
 
         chatsList.setItems(contactList);
 
@@ -52,25 +56,28 @@ public class Controller {
         chatsList.setOnMouseClicked(event -> chatArea.setItems(contacts.get(getCurrentContactIndex()).getMessages()));
 
         sendButton.setOnAction(event -> {
-            if (userMessage.getText().length() > 0) {
-                contacts.get(getCurrentContactIndex()).addMessage("You", userMessage.getText());
-                userMessage.clear();
-                System.out.println(getCurrentContactIndex());
-            }
+            sendMessage(contacts);
         });
         userMessage.setOnKeyPressed(event -> {
             if (event.getCode().equals(KeyCode.ENTER)) {
-                if (userMessage.getText().length() > 0) {
-                    contacts.get(getCurrentContactIndex()).addMessage("You", userMessage.getText());
-                    userMessage.clear();
-                    System.out.println(getCurrentContactIndex());
-                }
+                sendMessage(contacts);
             }
         });
 
     }
 
+    private void sendMessage(ObservableList<Contact> contacts) {
+        if (userMessage.getText().length() > 0) {
+            contacts.get(getCurrentContactIndex()).addMessage("You", userMessage.getText(), getCurrentDate());
+            userMessage.clear();
+        }
+    }
+
     private int getCurrentContactIndex() {
         return chatsList.getSelectionModel().getSelectedIndex();
+    }
+
+    private String getCurrentDate() {
+        return DateTimeFormatter.ofPattern("dd-MM-yy hh:mm:ss").format(LocalDateTime.now());
     }
 }
